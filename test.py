e36fb25be6c98c0f684a940dfaef0637e5941413
Dante Falzone <claire0000@protonmail.com>
2022-03-04 22:11:41 -0600

    wrote Pixel and PixelLayer classes

--- test.py
+++ test.py
@@ -2,11 +2,40 @@ import pygame
 from window import Window
 
 if __name__ == "__main__":
-  window = Window(500, 500)
+  window = Window(150, 150)
 
   def update(window, data):
-    window.screen.fill((0, 0, 0))
-    pygame.draw.circle(window.screen, (0, 255, 80), (250, 250), 75)
-    pygame.display.flip()
+    # midpoint circle algorithm
+    # copied from here https://rosettacode.org/wiki/Bitmap/Midpoint_circle_algorithm#Python
+    radius = 50
+    x0 = 75
+    y0 = 75
+    color = (0, 255, 80, 255)
+    f = 1 - radius
+    ddf_x = 1
+    ddf_y = -2 * radius
+    x = 0
+    y = radius
+    window.layers[0].set_pixel(x0, y0 + radius, color)
+    window.layers[0].set_pixel(x0, y0 - radius, color)
+    window.layers[0].set_pixel(x0 + radius, y0, color)
+    window.layers[0].set_pixel(x0 - radius, y0, color)
+
+    while x < y:
+      if f >= 0:
+        y -= 1
+        ddf_y += 2
+        f += ddf_y
+      x += 1
+      ddf_x += 2
+      f += ddf_x
+      window.layers[0].set_pixel(x0 + x, y0 + y, color)
+      window.layers[0].set_pixel(x0 - x, y0 + y, color)
+      window.layers[0].set_pixel(x0 + x, y0 - y, color)
+      window.layers[0].set_pixel(x0 - x, y0 - y, color)
+      window.layers[0].set_pixel(x0 + y, y0 + x, color)
+      window.layers[0].set_pixel(x0 - y, y0 + x, color)
+      window.layers[0].set_pixel(x0 + y, y0 - x, color)
+      window.layers[0].set_pixel(x0 - y, y0 - x, color)
 
   window.mainloop(update, {})
--- window.py
+++ window.py
@@ -1,5 +1,38 @@
 import pygame
 
+PIXEL_SIZE = 3
+
+class Pixel:
+  def __init__(self, x, y, color):
+    self.x = x
+    self.y = y
+    self.color = color
+
+
+class PixelLayer:
+  def __init__(self, width, height):
+    self.pixels = []
+    for x in range(width):
+      self.pixels.append([])
+      for y in range(width):
+        self.pixels[x].append(Pixel(x, y, (0, 0, 0, 0)))
+
+  def set_pixel(self, x, y, color):
+    self.pixels[x][y].color = color
+
+  def render(self, screen):
+    for col in self.pixels:
+      for pixel in col:
+        pygame.draw.rect(
+          screen,
+          pixel.color,
+          (
+            pixel.x * PIXEL_SIZE,
+            pixel.y * PIXEL_SIZE,
+            PIXEL_SIZE, PIXEL_SIZE
+          )
+        )
+
 
 class Window:
   def __init__(
@@ -8,7 +41,10 @@ class Window:
     height: int
   ):
     pygame.init()
-    self.screen = pygame.display.set_mode([width, height])
+    self.screen = pygame.display.set_mode(
+      [width*PIXEL_SIZE, height*PIXEL_SIZE]
+    )
+    self.layers = [PixelLayer(width, height)]
 
   def mainloop(
     self,
@@ -21,4 +57,7 @@ class Window:
         if event.type == pygame.QUIT:
           self.running = False
       update(self, data)
+      for layer in self.layers:
+        layer.render(self.screen)
+      pygame.display.flip()
     pygame.quit()
